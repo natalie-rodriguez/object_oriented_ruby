bdd4cf9c0a0cf481cf5b5c92d3b9e363f4aefbbb
natalie <nataliediazrodriguez@gmail.com>
2019-06-20 14:06:32 -0400

    first commit

---
+++ store_item.rb
@@ -0,0 +1,17 @@
+book1 = {
+  :title => "Art of War ",
+  :author => "Unknown",
+   :price => 20
+ }
+
+book2 = {
+  :title => "IT ",
+  :author => "Stephen King",
+   :price => 15
+ }
+
+book3 = {
+  :title => "Becoming ",
+  :author => "Michelle Obama",
+  :price => 25 
+ }''
